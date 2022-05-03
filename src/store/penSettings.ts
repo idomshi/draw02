1d48d0a211d36f8b5a5acb154d80566c783e2822
idomshi <56523771+idomshi@users.noreply.github.com>
2022-05-03 14:04:43 +0000

    ペン設定をLocalStorageに保存できるようになりました。

--- src/store/penSettings.ts
+++ src/store/penSettings.ts
@@ -1,15 +1,4 @@
 import { defineStore } from 'pinia'
-// import { useLocalStorage } from '@vueuse/core'
-
-// // localstorageで使うやつ。
-// const activePenColorKey = 'ACTIVE_PEN_COLOR'
-// type ActivePenColor = { deg: number; svPos: { x: number, y: number } }
-
-// // localstorageに保存されているイメージを読み込む。
-// const localStorage = useLocalStorage<ActivePenColor>(activePenColorKey, {
-//   deg: 0,
-//   svPos: { x: 0, y: 0 },
-// })
 
 export const usePenSettings = defineStore('penSettings', {
     state: (): { size: number; color: { deg: number; x: number; y: number } } => {
@@ -27,17 +16,19 @@ export const usePenSettings = defineStore('penSettings', {
         getPenSize (state) { return state.size },
     },
     actions: {
-        setColor (colorProps: { deg: number; x: number; y: number }) {
-            this.color.deg = colorProps.deg
-            this.color.x = colorProps.x
-            this.color.y = colorProps.y
-
-            localStorage.value.deg = colorProps.deg
-            localStorage.value.svPos.x = colorProps.x
-            localStorage.value.svPos.y = colorProps.y
+        setColor (colorProps: { deg?: number; x?: number; y?: number }) {
+            if (colorProps.deg !== undefined) { this.color.deg = colorProps.deg }
+            if (colorProps.x !== undefined) { this.color.x = colorProps.x }
+            if (colorProps.y !== undefined) { this.color.y = colorProps.y }
         },
         setSize (size: number) {
             this.size = size
         },
+    },
+    persist: {
+        enabled: true,
+        strategies: [
+            { storage: localStorage },
+        ],
     },
 })
